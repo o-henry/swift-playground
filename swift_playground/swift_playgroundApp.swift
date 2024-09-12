5e2a7eb4caf15e69d26fd72cb734a6633689c302
Henry <c.henry.9209@gmail.com>
2024-09-12 12:22:49 +0900

    [INIT]

---
+++ swift_playground/swift_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_playgroundApp.swift
+//  swift_playground
+//
+//  Created by Henry on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
